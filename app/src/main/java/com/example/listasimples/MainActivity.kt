ac22e6731a1409dded903d61cf0f1ed637e9e9da
I1DS <izalopesr21@gmail.com>
2024-05-07 15:48:53 -0300

    first commit

--- app/src/main/java/com/example/listasimples/MainActivity.kt
+++ app/src/main/java/com/example/listasimples/MainActivity.kt
@@ -7,45 +7,51 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isVisible
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val etnovatarefa = findViewById<EditText>(R.id.etnovatarefa)
-        val btadd = findViewById<Button>(R.id.btadd)
-        val lvtarefas = findViewById<ListView>(R.id.lvtarefas)
+        val etNovaTarefa = findViewById<EditText>(R.id.etnovatarefa)
+        val btAdd = findViewById<Button>(R.id.btadd)
+        val tvTitulo = findViewById<TextView>(R.id.tvtitulo)
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
 
-        //aqui criamos a lista de Strings, inicialmente vazia
+        fab.setOnClickListener {
+            tvTitulo.isVisible = false
+            etNovaTarefa.isVisible = true
+            etNovaTarefa.isEnabled = true
+            btAdd.isVisible = true
+        }
+
+        val lvTarefas = findViewById<ListView>(R.id.lvtarefas)
         val listaTarefas: ArrayList<String> = ArrayList()
-
-        //para trabalhar com listas, precisamos de um adapter
-        //um componente adicional do android para layout de listas
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaTarefas)
+        lvTarefas.adapter = adapter
 
-        //aqui o adapter do listview recebe o adapter que criamos
-        lvtarefas.adapter = adapter
-
-        btadd.setOnClickListener {
-            if (etnovatarefa.text.isNullOrEmpty()) {
+        btAdd.setOnClickListener {
+            if (etNovaTarefa.text.isNullOrEmpty()) {
                 Toast.makeText(this, "Digite uma tarefa...", Toast.LENGTH_SHORT).show()
             } else {
-                listaTarefas.add(etnovatarefa.text.toString())
-                //notificamos ao adaptar que tivemos alteração da lista
-                //notificado, ele atualiza os novos elementos da lista na tela
+                listaTarefas.add(etNovaTarefa.text.toString())
                 adapter.notifyDataSetChanged()
-                etnovatarefa.setText("")
+                etNovaTarefa.setText("")
+                etNovaTarefa.isVisible = false
+                etNovaTarefa.isEnabled = false
+                btAdd.isVisible = false
+                tvTitulo.isVisible = true
             }
         }
-        lvtarefas.setOnItemClickListener { _, _, position, _ ->
-            //aqui montamos a caixa de diálogo
-            val alerta = AlertDialog.Builder(this)
-            alerta.setTitle("Atenção!")
-            alerta.setMessage("Quer Mesmo Excluir Esse Item?")
-            alerta.setPositiveButton("Confirmar") { dialog, _ ->
 
-                //caso o botão confirmar seja clicado, remover o item da lista
+        lvTarefas.setOnItemLongClickListener { _, _, position, _ ->
+            val alerta = AlertDialog.Builder(this)
+            alerta.setTitle("Atenção")
+            alerta.setMessage("Quer mesmo excluir esse item?")
+            alerta.setPositiveButton("Confirmar") { dialog, _ ->
                 listaTarefas.removeAt(position)
                 adapter.notifyDataSetChanged()
                 dialog.dismiss()
@@ -58,4 +64,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
